032419a0a13b56afa601484b3a415d5e5c0aa7bf
ekta12345 <ektakumbhare3@gmail.com>
2021-04-08 18:34:07 +0530

    Added code for checking if the employee is part time or full time

--- Program.cs
+++ Program.cs
@@ -6,8 +6,8 @@ namespace EmployeeWageComputation
     {
         static void Main(string[] args)
         {
-            //constants
-            int present = 1;
+            int FULL_TIME = 1;
+            int PART_TIME = 2;
             int EMP_RATE_PER_HR = 20;
 
             //local variables
@@ -16,17 +16,23 @@ namespace EmployeeWageComputation
 
             //inbuilt class
             Random random = new Random();
-            int employeeCheck = random.Next(0, 2);
+            int employeeCheck = random.Next(0, 3);
 
             Console.WriteLine("random value " + employeeCheck);
             //Selection statements
-            if (employeeCheck == present)
+            if (employeeCheck == FULL_TIME)
             {
                 empHrs = 8;
+            }
+            else if (employeeCheck == PART_TIME)
+            {
+                empHrs = 4;
+
             }
             else
             {
                 empHrs = 0;
+
             }
             //Computation
             empWage = EMP_RATE_PER_HR * empHrs;
@@ -35,4 +41,4 @@ namespace EmployeeWageComputation
         }
 
     }
- }
+}
