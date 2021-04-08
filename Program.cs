ecedac4d193cefc7c2eebcb93cef38c58e99ceb6
ekta12345 <ektakumbhare3@gmail.com>
2021-04-08 18:21:18 +0530

    Added code for checking if Employee is present or absent

--- Program.cs
+++ Program.cs
@@ -6,8 +6,17 @@ namespace EmployeeWageComputation
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Employee Wage Computation Program");
-            Console.ReadLine();
+            int present = 1;
+
+            Random randomValue = new Random();
+            int employeeCheck = randomValue.Next(0, 2);
+
+            if (employeeCheck == present)
+                  Console.WriteLine("Employee is Present");
+            else
+                  Console.WriteLine("Employee is absent");
+                  Console.Read();
+              
         }
 
     }
