99534760ca91fa09ba998e8ce8efecb733b8db89
ekta12345 <ektakumbhare3@gmail.com>
2021-04-09 09:48:08 +0530

    Added code for calculating the wages till max hours or days is reached

--- Program.cs
+++ Program.cs
@@ -8,17 +8,23 @@ namespace EmployeeWageComputation
         public const int FULL_TIME = 1;
         public const int PART_TIME = 2;
         public const int EMP_RATE_PER_HR = 20;
-        public const int WORKING_DAYS = 20;
+        public const int MAX_WORKING_DAYS = 20;
+        public const int MAX_WORKING_HRS = 100;
+       
         static void Main(string[] args)
         {
             //local variables
             int empHrs = 0;
             int empWage = 0;
             int totalEmpwage = 0;
+            int workingDays = 1;
+            int hrs = 0;
+
             //inbuilt class
             Random random = new Random();
-            for (int day = 0; day < WORKING_DAYS; day++)
+            while( hrs < MAX_WORKING_HRS && workingDays <= MAX_WORKING_DAYS )
             {
+                workingDays++;
                 int employeeCheck = random.Next(0, 3);
 
                 //Console.WriteLine("random value " + employeeCheck);
@@ -41,11 +47,12 @@ namespace EmployeeWageComputation
                 }
                 //Computation 
                 empWage = EMP_RATE_PER_HR * empHrs;
-                
+                hrs += empHrs;
                 totalEmpwage += empWage;
                 
             }
-            Console.WriteLine("Employee wage for 20 days " + totalEmpwage);
+            Console.WriteLine("Employee wage for " + workingDays+ " days " +totalEmpwage);
+            Console.WriteLine("Working hours " + hrs);
             Console.Read();
        
         }
